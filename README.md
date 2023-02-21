b0993c767d8ab06470df8a1774c224eb484f81b5
elborracho420 <e.baxter@dark-coin.io>
2023-02-20 19:46:03 -0600

    Adding basic game structure to begin development.

---
+++ README.md
@@ -0,0 +1,5 @@
+#Asteroids
+
+This is an attempt to deploy a web based version of the traditional Asteroids game. 
+
+Will add link to deployed app once it is more functional.
---
+++ src/App.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import './App.css';
+
+const AsteroidsGame = () => {
+  const [player, setPlayer] = useState({
+    x: 0,
+    y: 0,
+    angle: 0,
+    velocity: { x: 0, y: 0 },
+  });
+  const [asteroids, setAsteroids] = useState([]);
+  const [bullets, setBullets] = useState([]);
+
+  useEffect(() => {
+    // Game loop logic
+    const gameLoop = () => {
+      // Update game state here
+    };
+
+    // Start game loop
+    const intervalId = setInterval(gameLoop, 16);
+
+    // Cleanup function
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const handleKeyDown = (event) => {
+    // Update game state based on user input
+  };
+
+  return (
+    <div
+      id="container"
+      style={{
+        position: "absolute",
+        width: "800px",
+        height: "600px",
+        backgroundColor: "#000",
+      }}
+      onKeyDown={handleKeyDown}
+      tabIndex={0}
+    >
+      <div
+        style={{
+          position: "absolute",
+          left: `${player.x + 400}px`,
+          top: `${player.y + 300}px`,
+          transform: `rotate(${player.angle}deg)`,
+          width: "20px",
+          height: "40px",
+          border: "1px solid #fff",
+        }}
+      />
+      {asteroids.map((asteroid) => (
+        <div
+          key={asteroid.id}
+          style={{
+            position: "absolute",
+            left: `${asteroid.x}px`,
+            top: `${asteroid.y}px`,
+            width: `${asteroid.size}px`,
+            height: `${asteroid.size}px`,
+            border: "1px solid #fff",
+            borderRadius: "50%",
+          }}
+        />
+      ))}
+      {bullets.map((bullet) => (
+        <div
+          key={bullet.id}
+          style={{
+            position: "absolute",
+            left: `${bullet.x}px`,
+            top: `${bullet.y}px`,
+            width: "2px",
+            height: "2px",
+            backgroundColor: "#fff",
+          }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default AsteroidsGame;
