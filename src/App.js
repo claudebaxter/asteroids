e18581642614fd11993fe3de92417253c2c906e2
elborracho420 <e.baxter@dark-coin.io>
2023-02-21 19:22:44 -0600

    Ship can move but needs optomized. Can shoot bullets with spacebar but not fully functional.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import './App.css';
+import "./App.css";
 
 const AsteroidsGame = () => {
   const [player, setPlayer] = useState({
@@ -16,17 +16,44 @@ const AsteroidsGame = () => {
     const gameLoop = () => {
       // Update game state here
       const xVelocity = player.velocity.x * 0.99;
-  const yVelocity = player.velocity.y * 0.99;
+      const yVelocity = player.velocity.y * 0.99;
 
-  // Update the player state with the new velocity
-  setPlayer((prevState) => ({
-    ...prevState,
-    x: prevState.x + xVelocity,
-    y: prevState.y + yVelocity,
-    velocity: { x: xVelocity, y: yVelocity },
-  }));
+      // Update the player state with the new velocity
+      setPlayer((prevState) => ({
+        ...prevState,
+        x: prevState.x + xVelocity,
+        y: prevState.y + yVelocity,
+        velocity: { x: xVelocity, y: yVelocity },
+      }));
+
+      // Update the bullets state with new positions
+      setBullets((prevState) =>
+        prevState
+          .map((bullet) => ({
+            ...bullet,
+            x: bullet.x + bullet.velocity.x,
+            y: bullet.y + bullet.velocity.y,
+            style: {
+              ...bullet.style,
+              color: "#fff",
+            },
+          }))
+          .filter(
+            (bullet) =>
+              bullet.x > -400 &&
+              bullet.x < 400 &&
+              bullet.y > -300 &&
+              bullet.y < 300 &&
+              !asteroids.some((asteroid) => isColliding(bullet, asteroid))
+          )
+      );
     };
 
+    //bullet impact collision / out of bounds handling:
+    const isColliding = (a, b) => {
+      const distance = Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2);
+      return distance < a.size / 2 + b.size / 2;
+    };
     // Start game loop
     const intervalId = setInterval(gameLoop, 16);
 
@@ -88,6 +115,25 @@ const AsteroidsGame = () => {
       velocity: { x: xVelocity, y: yVelocity },
     }));
   }
+
+  // Space bar key
+  if (keyCode === 32) {
+    if (bullets.length < 5) { // limit the number of bullets on the screen
+      const radians = (player.angle * Math.PI) / 180;
+    const bulletSpeed = 7;
+    const xVelocity = player.velocity.x + Math.sin(radians) * bulletSpeed;
+    const yVelocity = player.velocity.y - Math.cos(radians) * bulletSpeed;
+    const xOffset = Math.sin(radians) + 400; // add an offset to the x position
+    const yOffset = -Math.cos(radians) + 250; // add an offset to the y position
+    const x = player.x + xOffset; // calculate the new x position
+    const y = player.y + yOffset; // calculate the new y position
+
+    setBullets((prevState) => [
+      ...prevState,
+      { id: Date.now(), x, y, velocity: { x: xVelocity, y: yVelocity } },
+    ]);
+    }
+  }
   };
 
   return (
@@ -130,20 +176,29 @@ const AsteroidsGame = () => {
           }}
         />
       ))}
-      {bullets.map((bullet) => (
-        <div
-          key={bullet.id}
-          style={{
-            position: "absolute",
-            left: `${bullet.x}px`,
-            top: `${bullet.y}px`,
-            width: "2px",
-            height: "2px",
-            backgroundColor: "#fff",
-          }}
-        />
+      {bullets.map(({ id, x, y }) => (
+        <Bullet key={id} x={x} y={y} size={5} style={{ color: "#fff" }} />
       ))}
     </div>
+  );
+};
+
+const Bullet = ({ x, y }) => {
+  const bulletSize = 10;
+
+  return (
+    <div
+      className="bullet"
+      style={{
+        position: "absolute",
+        top: y - bulletSize / 2,
+        left: x - bulletSize / 2,
+        width: bulletSize,
+        height: bulletSize,
+        borderRadius: bulletSize / 2,
+        backgroundColor: "#fff",
+      }}
+    />
   );
 };
 
